09a1cfbaa0fb180686c8961ce02c3ab0a756c653
Toine MARLANGE <marlange.toine@protonmail.com>
2023-02-02 17:52:12 +0400

    JS done , css responsive done , last chekup to do , and minify JS

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,25 +1,74 @@
 let result = 0;
 let score = 0;
-const dice3 = document.createElement('img');
-dice3.src = "/assets/images/dice3.png";
+let activePlayer = 0;
+let currentScore = [0, 0];
+
+const screenWidth = window.innerWidth;
+
+function rollDice(){  
+    let current = document.getElementById(`current${activePlayer}`)
+    score = Math.floor(Math.random() * 6) + 1 
+        
+    if(score === 1){
+        current.textContent = 0;
+        changePlayer();
+        swal('Perdu!', 'Joueur'+ (activePlayer + 1) +' à vous !', 'warning')
+    
+    }else{
+        result += score;
+        current.textContent = result;
+        if (screenWidth > 768){
+            dice.innerHTML = `<img src="/assets/images/dice${score}.png" alt="dice ${score}">`;
+        } 
+    }        
+}
+
+
+
+function holdFunction(){
+    let playerScore = document.getElementById(`scorep${activePlayer}`);
+    currentScore[activePlayer] += result;
+    playerScore.textContent = currentScore[activePlayer];
+
+    if (currentScore[activePlayer] >= 100){
+        rolldice.disabled = true;
+        hold.disabled = true;
+        newgamebtn.classList.add('newgameAnimation');
+        swal('Félicitation au joueur ' + (activePlayer +1) +'!!!!!', 'success');
+    
+    }else{
+        changePlayer();
+        swal('Score sauvegardé !', 'Joueur' +(activePlayer + 1) + ' à vous !', 'success');
+    } 
+}
+
+
+function changePlayer(){
+    result= 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0.classList.toggle("active-player");
+    player1.classList.toggle("active-player");
+    play1.classList.toggle("active-color");
+    play2.classList.toggle("active-color");
+};
 
 
 
 rolldice.addEventListener('click', ()=>{
-    score = Math.floor(Math.random() * 6) + 1 
-    if(score === 1){
-        result= 0;
-    }else{
-        result = result + score;
-    }
-
-
-    // INSERER IMAGE A CHAQUE LANCER 
-    // scorep1.textContent = result;
-    // if (score = 3){
-    //   dice.innerHtml= dice3  
-    // }
-    
+    rollDice();
 })
 
+
+
+hold.addEventListener('click', () =>{
+    let current = document.getElementById(`current${activePlayer}`)
+    holdFunction();
+    current.textContent = 0;
+})
+
+
+
+newgame.addEventListener('click', ()=>{
+    location.reload();
+})
 
